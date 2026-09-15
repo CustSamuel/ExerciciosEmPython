9fafc3f4f24998a354be45e904577b938b011699
Samuel Custódio <samuelcustodiosantos2020@gmail.com>
2026-09-15 16:20:20 -0300

    Create sistemalogin.py
    Este script simula um sistema de validação de acesso que retém o usuário em uma estrutura de repetição (while) enquanto a senha digitada for incorreta, bloqueando o avanço e solicitando o dado novamente até que o código numérico correto (1234) seja inserido para liberar o sistema.

---
+++ sistemalogin.py
@@ -0,0 +1,9 @@
+print('======= BEM VINDO =======')
+
+senha = int(input('Digite a sua senha: \n'))
+
+while senha != 1234:
+  print('Senha incorreta. Tente novamente.')
+  senha = int(input('Digite a sua senha: \n'))
+
+print('Senha correta. Bem-vindo!')
